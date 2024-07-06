dd239e552e020f27439342cbb82fcf6514f035b5
FedorProg <FedorGermanGut@gmail.com>
2024-07-06 15:36:09 +0300

    Update ThemeClassOOP.cpp
    Basic

--- ThemeClassOOP.cpp
+++ ThemeClassOOP.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,9 +8,14 @@ int main()
 	class ClassName
 	{
 		// Поля класса (данные)
-
+		int age;
+		string name;
 		// Методы класса (функции)
 	};	
+
+	// ClassName* ObjectName = new ClassName(); - Создание нового объекта №1
+	// ClassName ObjectName; -  Создание нового объекта №2
+	// delete ObjectName;
 
 	return 0;
 }
